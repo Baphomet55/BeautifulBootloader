f5ebaab884b3491ee816686dcf15e74b091cb32c
Steve Harvey <Zabbaniyya@proton.me>
2025-07-11 15:56:52 +0000

    Testing

--- src/boot.c
+++ src/boot.c
@@ -1,4 +1,27 @@
+asm(
+".data\n"
+"dummy:\n"
+".long 0\n"
+".section .reloc\n"
+<<<<<<< Updated upstream
+"mock_entry:\n"
+".long dummy-mock_entry\n"
+".long 10\n"
+".word 0\n"
+".text\n"
+);
+
+int main()
+=======
+"mock_rel:\n"
+".long mock_rel-dummy\n"
+".long 10\n"
+".word 0\n"
+);
+
 int boot()
+>>>>>>> Stashed changes
 {
+	while(true) {;}
 	return 0;
 }
