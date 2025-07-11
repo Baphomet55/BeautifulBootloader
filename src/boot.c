cc10e218c90996d2acbb7f7bbd4885eeba9a61da
Steve Harvey <Zabbaniyya@proton.me>
2025-07-11 16:54:14 +0000

    Adding a .gitignore file

--- src/boot.c
+++ src/boot.c
@@ -1,9 +1,10 @@
+#include "uefi.h"
+
 asm(
 ".data\n"
 "dummy:\n"
 ".long 0\n"
 ".section .reloc\n"
-<<<<<<< Updated upstream
 "mock_entry:\n"
 ".long dummy-mock_entry\n"
 ".long 10\n"
@@ -11,17 +12,38 @@ asm(
 ".text\n"
 );
 
-int main()
-=======
-"mock_rel:\n"
-".long mock_rel-dummy\n"
-".long 10\n"
-".word 0\n"
-);
-
-int boot()
->>>>>>> Stashed changes
+uint64 debug(uint64 val)
 {
+	return val;
+}
+
+int main(void* handle, EFI_SYSTEM_TABLE* system_table)
+{
+	uintn memory_map_size = 0;
+	EFI_MEMORY_DESCRIPTOR* memory_map;
+	uintn map_key = 0;
+	uintn descriptor_size = 0;
+	uint32 descriptor_version = 0;
+
+	system_table->BootServices->GetMemoryMap(
+		&memory_map_size,
+		memory_map,
+		&map_key,
+		&descriptor_size,
+		&descriptor_version
+	);
+
+	memory_map_size += 2*descriptor_size;
+
+	system_table->BootServices->AllocatePool(
+		EfiLoaderData,
+		memory_map_size,
+		(void**) &memory_map
+	);
+	debug((uint64) memory_map);
 	while(true) {;}
+	//system_table->BootServices->GetMemoryMap();
+	//system_table->BootServices->ExitBootServices();
+	
 	return 0;
 }
